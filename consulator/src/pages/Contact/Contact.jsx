23e366a78384a5ee7590fef1f67f3e9fb4d8983a
Cümşüd İsmayılov <c.smayilov1@gmail.com>
2023-08-03 07:03:45 +0400

    contact page add

--- consulator/src/pages/Contact/Contact.jsx
+++ consulator/src/pages/Contact/Contact.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import "../Contact/Contact.scss"
 import ContactCards from '../../components/ContactCards/ContactCards';
+import Buttons from "../../components/ButtonComponent/Buttons";
+
 const Contact = () => {
   return (
     <main>
@@ -27,10 +29,47 @@ const Contact = () => {
                     <p>Have Any Question?</p>
                     <h2>It is a long established fact that a reader will be distracted content of a page when looking.</h2>
                  </div>
+                 <div className='contact-form-area'>
+                   <form>
+                    <div className='row justify-content-center'>
+                      <div className='col-xl-6'>
+                         <div className='form-field'>
+                            <input type="text" name="name" id="name" className='name' placeholder='Name' />
+                         </div>
+                      </div>
+                      <div className='col-xl-6'>
+                         <div className='form-field'>
+                            <input type="text" name="lastname" id="lastname" className='lastname' placeholder='Lastname' />
+                         </div>
+                      </div>
+                      <div className='col-xl-6'>
+                         <div className='form-field'>
+                            <input type="email" name="emaills" id="emaills" className='emailss' placeholder='Email' />
+                         </div>
+                      </div>
+                      <div className='col-xl-6'>
+                         <div className='form-field'>
+                            <input type="text" name="subject" id="subject" className='subject' placeholder='Subject' />
+                         </div>
+                      </div>
+                      <div className='col-xl-12'>
+                        <div className='form-field'>
+                           <textarea name="message" className='textarea' id="message" cols="30" rows="10" placeholder='Message'></textarea>
+                        </div>
+                      </div>
+                      <div className='col-xl-12'>
+                         <div className='form-submit'>
+                            <Buttons type="text" className="sendmessage"  tittle="Send Message" />
+                         </div>
+                      </div>
+                    </div>
+                   </form>
+                 </div>
              </div>
           </div>
         </div>
      </section>
+   
     </main>
   )
 }
