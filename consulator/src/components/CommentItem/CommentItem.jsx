30555a072f60612480e9c7bc103ecdbe72f46d55
Cümşüd İsmayılov <c.smayilov1@gmail.com>
2023-08-05 09:11:47 +0400

    commit item add

---
+++ consulator/src/components/CommentItem/CommentItem.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "../CommentItem/CommentItem.scss";
+
+const CommentItem = () => {
+  const [comments, setComments] = useState([]);
+  const BASE_URL = "http://localhost:3001/";
+  useEffect(() => {
+    fetch(`${BASE_URL}comments-data`)
+      .then((response) => response.json())
+      .then((data) => {
+        setComments(data);
+      });
+  }, []);
+
+  return (
+    <div id="comments-item">
+      {comments.length > 0 &&
+        comments.map((element, index) => (
+          <div key={index} className="comment-cards">
+            <img src={element.image} alt="imgs" />
+            <div className="commentstext">
+              <div className="nameanddate">
+                <p>{element.name}</p>
+                <h3>{element.date}</h3>
+              </div>
+              <h4>{element.tittle1}</h4>
+              <h5>{element.tittle2}</h5>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default CommentItem;
