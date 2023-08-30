822443951dff58a73b30364f00275a4db50c693b
Cümşüd İsmayılov <c.smayilov1@gmail.com>
2023-08-30 08:00:09 +0400

    accordion

--- consulator/src/components/AccordionItem/Accordion.jsx
+++ consulator/src/components/AccordionItem/Accordion.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
@@ -6,85 +7,35 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function BasicAccordion() {
+  const [accordion, setAccordion] = useState([]);
+  const BASE_URL = "http://localhost:3001/";
+  useEffect(() => {
+    fetch(`${BASE_URL}accordionitem`)
+      .then((response) => response.json())
+      .then((data) => {
+        setAccordion(data);
+      });
+  }, []);
   return (
     <section id="accordion-item">
       <div className="container">
         <div className="row">
           <div className="col-xl-8">
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>
-                  Market research on our global panel with support from our
-                  experts.
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-                  exercitationem pariatur iure nemo esse repellendus est quo
-                  recusandae. Delectus, maxime.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
-              >
-                <Typography>
-                  Planning can help alleviate workplace stress and increase
-                  productivity.
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-                  exercitationem pariatur iure nemo esse repellendus est quo
-                  recusandae. Delectus, maxime.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3a-content"
-                id="panel3a-header"
-              >
-                <Typography>
-                  Those who experiment the most, are able to innovate the best.
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-                  exercitationem pariatur iure nemo esse repellendus est quo
-                  recusandae. Delectus, maxime.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel4a-content"
-                id="panel4a-header"
-              >
-                <Typography>
-                  Understand Your Problem, You must understand the issue.
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-                  exercitationem pariatur iure nemo esse repellendus est quo
-                  recusandae. Delectus, maxime.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
+            {accordion.length > 0 &&
+              accordion.map((element, index) => (
+                <Accordion key={index}>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography>{element.tittle}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>{element.desc}</Typography>
+                  </AccordionDetails>
+                </Accordion>
+              ))}
           </div>
         </div>
       </div>
