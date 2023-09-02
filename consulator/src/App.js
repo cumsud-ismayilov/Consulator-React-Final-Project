3260414d137140c15179dede4e5b24564a3f2d5b
Cümşüd İsmayılov <c.smayilov1@gmail.com>
2023-09-02 13:34:56 +0400

    add

--- consulator/src/App.js
+++ consulator/src/App.js
@@ -3,30 +3,27 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import "../src/style/global.scss";
 import routes from "./router";
 import Layout from "./components/Layout/Layout";
-import ScrollToTop  from "./components/ScrollToTop/ScrollToTop"
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import ScrooltotopOnpageChange from "./components/ScrollToTopOnPageChange/ScrollToTopOnPageChange";
 const App = () => {
   const location = useLocation();
   console.log(`location`, location);
-  return location.pathname === "/login" ||location.pathname==="/register"? (
-   
-      <Routes>
-        {routes.length > 0 &&
-          routes.map((element) => (
-            
-            <Route
-              key={element.id}
-              index
-              path={element.path}
-              element={element.element}
-            />
-          ))}
-      </Routes>
-    
+  return location.pathname === "/login" || location.pathname === "/register" ? (
+    <Routes>
+      {routes.length > 0 &&
+        routes.map((element) => (
+          <Route
+            key={element.id}
+            index
+            path={element.path}
+            element={element.element}
+          />
+        ))}
+    </Routes>
   ) : (
     <Layout>
-      <ScrooltotopOnpageChange/>
-      <ScrollToTop/>
+      <ScrooltotopOnpageChange />
+      <ScrollToTop />
       <Routes>
         {routes.length > 0 &&
           routes.map((element) => (
--- consulator/src/index.js
+++ consulator/src/index.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter} from "react-router-dom"
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.min";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  
     <App />
-  
   </BrowserRouter>
 );
-
-
